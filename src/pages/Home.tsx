c66f6ba3c1207a26a24cf8e3effc5b0a9efa7b6c
urfrider <wnsl9704@naver.com>
2023-02-16 21:06:03 +0800

    adjusted home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,17 +29,7 @@ function Home({ setCurrentPage }: IHomeProps) {
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
     >
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
-        {isGTMediumScreen ? (
-          <div
-            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 
-            before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 
-          before:border-blue before:z-[-1]"
-          >
-            <ProfileImg />
-          </div>
-        ) : (
-          <ProfileImg />
-        )}
+        <ProfileImg />
       </div>
       <div className="z-30 basis-2/5 mt-12 md:mt-32">
         <motion.div
